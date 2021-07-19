7f076fa32a413cf0ec8d54344ba88465f356e9c4
khoirulbads <irulad151@gmail.com>
2021-07-19 11:01:11 +0700

    urutkan obat kategori terlaris di grafik

--- resources/views/pemilik/dashboard.blade.php
+++ resources/views/pemilik/dashboard.blade.php
@@ -176,7 +176,7 @@ $penjualan = DB::select("select sum(qty) as c from detail_transaksi");
 $datausia = DB::select("SELECT  count(id_obat) as c from obat where TIMESTAMPDIFF(DAY,now(),tgl_kadaluarsa) <= selisih and aktif=1");
 $datastok = DB::select("SELECT  count(id_obat) as c from obat where stok <= stokMinimal and aktif=1");
 $datakad = DB::select("SELECT  count(id_obat) as c from obat where TIMESTAMPDIFF(DAY,now(),tgl_kadaluarsa) <= 0 and aktif=1");
-$chart1 = DB::select("select c.kategori, sum(a.qty) as qty, MONTHNAME(now()) as bulan from detail_transaksi a, obat b, kategori c where c.id_kategori=b.id_kategori group by c.id_kategori");
+$chart1 = DB::select("select c.kategori, sum(a.qty) as qty, MONTHNAME(now()) as bulan from detail_transaksi a, obat b, kategori c where c.id_kategori=b.id_kategori group by c.id_kategori order by sum(a.qty) DESC");
 $pendapatan = DB::select("SELECT sum(grand_total) as c FROM transaksi WHERE MONTH(tanggal) = MONTH(now())");
 $laba = DB::select("select sum(a.harga_jual-a.harga_beli) as c from detail_transaksi a, transaksi b where a.inv=b.inv AND MONTH(b.tanggal)=MONTH(now())");
 $lababersih = array("","","","","","","");
@@ -278,9 +278,13 @@ for ($i=0; $i <= 6 ; $i++) {
       </div>
       <div class="row">
         <div class="col-lg-9 col-xs-6">
-            <canvas id="chartlaba"></canvas>
+            <canvas class="chart" id="chartlaba"></canvas>
         </div>
       </div>
+      <div class="box-body chart-responsive">
+              <div class="chart" id="line-chart" style="height: 300px;"></div>
+      </div>
+            
     
       </section>
       <!-- /.row -->
@@ -555,6 +559,90 @@ for ($i=0; $i <= 6 ; $i++) {
 <!-- ./wrapper -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+<!-- <script>
+  $(function () {
+    "use strict";
+
+    // AREA CHART
+    var area = new Morris.Area({
+      element: 'revenue-chart',
+      resize: true,
+      data: [
+        {y: '2011 Q1', item1: 2666, item2: 2666},
+        {y: '2011 Q2', item1: 2778, item2: 2294},
+        {y: '2011 Q3', item1: 4912, item2: 1969},
+        {y: '2011 Q4', item1: 3767, item2: 3597},
+        {y: '2012 Q1', item1: 6810, item2: 1914},
+        {y: '2012 Q2', item1: 5670, item2: 4293},
+        {y: '2012 Q3', item1: 4820, item2: 3795},
+        {y: '2012 Q4', item1: 15073, item2: 5967},
+        {y: '2013 Q1', item1: 10687, item2: 4460},
+        {y: '2013 Q2', item1: 8432, item2: 5713}
+      ],
+      xkey: 'y',
+      ykeys: ['item1', 'item2'],
+      labels: ['Item 1', 'Item 2'],
+      lineColors: ['#a0d0e0', '#3c8dbc'],
+      hideHover: 'auto'
+    });
+
+    // LINE CHART
+    var line = new Morris.Line({
+      element: 'chartlaba',
+      resize: true,
+      data: [
+        {y: '2011 Q1', item1: 2666},
+        {y: '2011 Q2', item1: 2778},
+        {y: '2011 Q3', item1: 4912},
+        {y: '2011 Q4', item1: 3767},
+        {y: '2012 Q1', item1: 6810},
+        {y: '2012 Q2', item1: 5670},
+        {y: '2012 Q3', item1: 4820},
+        {y: '2012 Q4', item1: 15073},
+        {y: '2013 Q1', item1: 10687},
+        {y: '2013 Q2', item1: 8432}
+      ],
+      xkey: 'y',
+      ykeys: ['item1'],
+      labels: ['Item 1'],
+      lineColors: ['#3c8dbc'],
+      hideHover: 'auto'
+    });
+
+    //DONUT CHART
+    var donut = new Morris.Donut({
+      element: 'sales-chart',
+      resize: true,
+      colors: ["#3c8dbc", "#f56954", "#00a65a"],
+      data: [
+        {label: "Download Sales", value: 12},
+        {label: "In-Store Sales", value: 30},
+        {label: "Mail-Order Sales", value: 20}
+      ],
+      hideHover: 'auto'
+    });
+    //BAR CHART
+    var bar = new Morris.Bar({
+      element: 'bar-chart',
+      resize: true,
+      data: [
+        {y: '2006', a: 100, b: 90},
+        {y: '2007', a: 75, b: 65},
+        {y: '2008', a: 50, b: 40},
+        {y: '2009', a: 75, b: 65},
+        {y: '2010', a: 50, b: 40},
+        {y: '2011', a: 75, b: 65},
+        {y: '2012', a: 100, b: 90}
+      ],
+      barColors: ['#00a65a', '#f56954'],
+      xkey: 'y',
+      ykeys: ['a', 'b'],
+      labels: ['CPU', 'DISK'],
+      hideHover: 'auto'
+    });
+  });
+</script>
+ -->
 <script>
     var user = [<?php foreach ($chart1 as $key) { ?>
             '<?php echo $key->kategori ?>',
@@ -636,6 +724,7 @@ for ($i=0; $i <= 6 ; $i++) {
 
 </script>
 
+<script src="assets/AdminLTE/bower_components/morris.js/morris.min.js"></script>
 
 <!-- jQuery 3 -->
 <script src="assets/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
