4df1f5fee2e2d2355a7a542c37e3346357d0965c
khoirulbads <irulad151@gmail.com>
2021-07-21 10:35:55 +0700

    grafik laba di kasir

--- resources/views/kasir/dashboard.blade.php
+++ resources/views/kasir/dashboard.blade.php
@@ -552,7 +552,17 @@ $chart1 = DB::select("select c.kategori, sum(a.qty) as qty, MONTHNAME(now()) as 
         labels: minggu,
         datasets: [{
             label: 'Laba Bersih Harian pada Minggu ini (Rupiah)',
-            backgroundColor: "orange",
+            borderColor: "#80b6f4",
+            pointBorderColor: "#80b6f4",
+            pointBackgroundColor: "#80b6f4",
+            pointHoverBackgroundColor: "#80b6f4",
+            pointHoverBorderColor: "#80b6f4",
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 3,
+            fill: false,
+            borderWidth: 4,
             data: laba
         }]
     };
@@ -599,10 +609,28 @@ $chart1 = DB::select("select c.kategori, sum(a.qty) as qty, MONTHNAME(now()) as 
                     display: true,
                 },scales: {
                     yAxes: [{
-                        ticks: {
-                        beginAtZero:true
-                        }
-                    }]
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+              }],
+              xAxes: [{
+                gridLines: {
+                    zeroLineColor: "transparent"
+},
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
+            }]
                 }
             }
         });
